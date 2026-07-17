ed259087644cd0133e8abf7e2048594e5791ac5f
Ruwaidah Alfakhri <ruwaidah.riyadh@gmail.com>
2026-07-17 12:19:42 -0500

    adding demo user

--- knexfile.js
+++ knexfile.js
@@ -6,13 +6,33 @@ dotenv.config();
  */
 module.exports = {
 
+  // development: {
+  //   client: "postgresql",
+  //   useNullAsDefault: true,
+  //   connection: {
+  //     database: process.env.DB_DATABASE_NAME,
+  //     user: "postgres",
+  //     password: process.env.DEVELOPMENT_DB_PASSWORD,
+  //   },
+  //   pool: {
+  //     min: 0,
+  //     max: 20,
+  //   },
+  //   migrations: {
+  //     directory: "./db/migrations",
+  //   },
+  //   seeds: {
+  //     directory: "./db/seeds",
+  //   },
+  // },
+
   development: {
-    client: "postgresql",
-    useNullAsDefault: true,
+    client: "pg",
     connection: {
-      database: process.env.DB_DATABASE_NAME,
-      user: "postgres",
-      password: process.env.DEVELOPMENT_DB_PASSWORD,
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
     pool: {
       min: 0,
@@ -20,12 +40,12 @@ module.exports = {
     },
     migrations: {
       directory: "./db/migrations",
+      tableName: "knex_migrations",
     },
     seeds: {
       directory: "./db/seeds",
     },
   },
-
 
   production: {
     client: "pg",
